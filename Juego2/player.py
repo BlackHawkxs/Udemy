257dbb7a3cdcbecdbff42494f906d5b9f5ec07ec
BlackHawkxs <cyber388@gmail.com>
2020-03-24 02:26:35 -0300

    tyu

--- Juego2/player.py
+++ Juego2/player.py
@@ -6,7 +6,7 @@ class Player:
     def __init__(self,**kwargs):
         self.hitpoints = kwargs.get('hitpoint',50)
         self.mana = kwargs.get('mana',50)
-        self.name = input('Escribe tu nombre')
+        self.name = input('Escribe tu nombre: ')
 
     def __str__(self):
         return '{} es un {} tiene {} puntos de vida y {} de mana, puede lanzar {} y moverse a {} clicks'.format(self.name,
